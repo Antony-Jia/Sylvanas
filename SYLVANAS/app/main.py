de1c7fb4961ee5b5c9bd41476bb354f33a33c91a
Antony-Jia <527223709@qq.com>
2022-09-23 17:00:40 +0800

    add openapi swagger

--- SYLVANAS/app/main.py
+++ SYLVANAS/app/main.py
@@ -6,6 +6,14 @@ from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 from app.api.api import api_router
 from app.core import config
+from fastapi.openapi.utils import get_openapi
+
+from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
 
 app = FastAPI(
     title=config.settings.PROJECT_NAME,
@@ -15,6 +23,8 @@ app = FastAPI(
     docs_url="/",
 )
 app.include_router(api_router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Sets all CORS enabled origins
 app.add_middleware(
@@ -26,4 +36,49 @@ app.add_middleware(
 )
 
 # Guards against HTTP Host Header attacks
-app.add_middleware(TrustedHostMiddleware, allowed_hosts=config.settings.ALLOWED_HOSTS)
+#app.add_middleware(TrustedHostMiddleware, allowed_hosts=config.settings.ALLOWED_HOSTS)
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Custom title",
+        version="2.5.0",
+        description="This is a very custom OpenAPI schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js",
+    )
+
+
+
+app.openapi = custom_openapi
